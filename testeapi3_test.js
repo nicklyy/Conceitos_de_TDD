25e7af60837c4a59d54df81555a67a0165599bb9
nicklyy <85380409+nicklyy@users.noreply.github.com>
2022-06-24 08:01:54 -0300

    Update testeapi3_test.js

--- testeapi3_test.js
+++ testeapi3_test.js
@@ -1,24 +1,35 @@
-import { assertThrows } from "https://deno.land/std@0.145.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertStrictEquals,
+  assertThrows,
+} from "https://deno.land/std@0.145.0/testing/asserts.ts";
+import { User } from "https://deno.land/std@0.145.0/testing/bdd_examples/user.ts";
 
-Deno.test("doesThrow", function (): void {
-  assertThrows((): void => {
-    throw new TypeError("hello world!");
-  });
-  assertThrows((): void => {
-    throw new TypeError("hello world!");
-  }, TypeError);
-  assertThrows(
-    (): void => {
-      throw new TypeError("hello world!");
-    },
-    TypeError,
-    "hello",
-  );
+Deno.test("User.users initially empty", () => {
+  assertEquals(User.users.size, 0);
 });
 
-// This test will not pass.
-Deno.test("fails", function (): void {
-  assertThrows((): void => {
-    console.log("Hello world");
+Deno.test("User constructor", () => {
+  try {
+    const user = new User("Kyle");
+    assertEquals(user.name, "Kyle");
+    assertStrictEquals(User.users.get("Kyle"), user);
+  } finally {
+    User.users.clear();
+  }
+});
+
+Deno.test("User age", async (t) => {
+  const user = new User("Kyle");
+
+  await t.step("getAge", () => {
+    assertThrows(() => user.getAge(), Error, "Age unknown");
+    user.age = 18;
+    assertEquals(user.getAge(), 18);
+  });
+
+  await t.step("setAge", () => {
+    user.setAge(18);
+    assertEquals(user.getAge(), 18);
   });
 });
